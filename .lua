f27eed48aa7a78f8b60dea4a7a6f4bf46432d61e
CloudyEX <ilhamfadil7745@gmail.com>
2026-08-25 21:04:38 +0700

    Add 'Grow a Garden 2' to the scripts list

--- .lua
+++ .lua
@@ -5,6 +5,7 @@ local Scripts = {
     [126884695634066] = { name = "QH Community",     url = "" },
     [83369512629707]  = { name = "Quantum Sawah",    url = "" },
     [139802517550914] = { name = "99 Night At Sea",  url = "https://raw.githubusercontent.com/CloudyEX/CloudyMain/refs/heads/main/99inthesea.lua" },
+    [10200395747] = { name = "Grow a Garden 2", url = "" },
 }
 
 local PlaceId = game.PlaceId
